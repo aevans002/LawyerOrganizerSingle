2e16263a9649ac9404258b024119c9161cefcd23
aevans002 <allanevans2005@msn.com>
2015-05-31 11:04:22 -0600

    Initial Commit

---
+++ LawyerOrganizerSingle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LawyerOrganizerSingle
+//
+//  Created by Administrator on 5/31/15.
+//  Copyright (c) 2015 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LawyerOrganizerSingle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LawyerOrganizerSingle
+//
+//  Created by Administrator on 5/31/15.
+//  Copyright (c) 2015 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
